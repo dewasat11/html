c133189852b703ebda3128f6ca2284b02b42d7a0
dewasat11 <dewasatriaaa@gmail.com>
2023-11-10 21:55:47 +0700

    chechk

--- pertemuan_10/tugas.php
+++ pertemuan_10/tugas.php
@@ -2,3 +2,18 @@ menghitung luas :
 - persegi panjang
 - segitiga
 - lingkaran
+
+<?php
+//rumus persegi panjang
+$panjang;
+$lebar;
+
+//tampilan data
+echo "panjang\n";
+echo "=========\n";
+$panjang = fgets(STDIN);
+echo "lebar" ,  $panjang;
+$nilai = fgets(STDIN);
+
+
+?>
